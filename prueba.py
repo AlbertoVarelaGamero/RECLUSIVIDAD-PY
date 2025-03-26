1039ab7f0d4d766923953f811b1a2f6df0f884d2
AlbertoVarelaGamero <albertovarelagamero@gmail.com>
2025-03-26 09:52:15 +0100

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,364 @@
+
+# hola
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+
+## COMO CREAR MATRICES
+
+
+
+## como crear una matriz
+
+filas = 3
+columnas = 3
+matriz = []
+
+for i in range(filas) :
+    fila = []
+    for j in range(columnas):
+        fila.append(i * columnas + j + 1)
+        matriz.append(fila)
+
+for fila in matriz:
+    print(fila)
+
+
+## crear una matriz solicitando al usuario
+
+filas = int(input("introduce el numero de filas:"))
+columnas = int(input("introduce el numero de columnas :"))
+matriz = []
+
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        numero = float(input("introduce un valor para el elemento {} de la fila {}:".format(j,i)))
+        fila.append(numero)
+    matriz.append(fila)
+
+print(matriz)
+
+

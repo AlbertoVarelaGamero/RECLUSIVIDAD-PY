2d652110c059e04a9832e63f5ed71842270dc84f
AlbertoVarelaGamero <albertovarelagamero@gmail.com>
2025-03-26 09:42:30 +0100

    Add files via upload

---
+++ reclusividad.py
@@ -0,0 +1,14 @@
+
+def factorial(numero):
+    if numero == 0:
+        return 1
+    else:
+        return numero * factorial(numero - 1)
+        print(f"factorial({numero}) = {resultado}")
+        return resultado
+    
+
+## Ejemplo de uso
+
+n = 5
+print(f"fibonnaci de {n} es {factorial(n)}")
